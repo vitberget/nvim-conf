0b03c09f012829ea66483f73f9255b4a95e3558b
Kenneth Hedman <kenneth.hedman@firefly.se>
2023-10-30 11:18:12 +0100

    rust tools shortcuts

---
+++ after/ftplugin/rust/rust-tools.lua
@@ -0,0 +1,9 @@
+vim.keymap.set("n", "<leader>Rr", ":RustRunnables<CR>" )
+vim.keymap.set("n", "<leader>Rd", ":RustDebuggables<CR>" )
+
+vim.keymap.set("n", "<leader>Rmiu", ":RustMoveItemUp<CR>" )
+vim.keymap.set("n", "<leader>Rmid", ":RustMoveItemDown<CR>" )
+
+vim.keymap.set("n", "<leader>Rha", ":RustHoverActions<CR>" )
+vim.keymap.set("n", "<leader>Rjl", ":RustJoinLines<CR>" )
+
