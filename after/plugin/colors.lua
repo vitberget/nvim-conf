e338372ab2a725d5fb1303ce7f5468e8c9939873
Kenneth Hedman <kenneth.hedman@firefly.se>
2023-11-14 10:41:04 +0100

    CursorColumn color

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -12,6 +12,7 @@ function ColorDark(color)
 
     vim.api.nvim_set_hl(0, "Cursor", { fg="orange", bg="orange" })
     vim.api.nvim_set_hl(0, "Cursor2", { fg="orange", bg="orange" })
+    vim.api.nvim_set_hl(0, "CursorColumn", { bg="#171000" })
 
     vim.opt.guicursor = "n-v-c:block-Cursor/lCursor,i-ci-ve:ver25-Cursor2/lCursor2,r-cr:hor20,o:hor50"
 
@@ -24,6 +25,12 @@ function ColorLight(color)
     vim.opt.termguicolors = true
     vim.cmd('set background=light')
     vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Cursor", { fg="orange", bg="orange" })
+    vim.api.nvim_set_hl(0, "Cursor2", { fg="orange", bg="orange" })
+    vim.api.nvim_set_hl(0, "CursorColumn", { bg="#171000" })
+
+    vim.opt.guicursor = "n-v-c:block-Cursor/lCursor,i-ci-ve:ver25-Cursor2/lCursor2,r-cr:hor20,o:hor50"
 end
 
 vim.api.nvim_create_user_command( 'ColorDark',
