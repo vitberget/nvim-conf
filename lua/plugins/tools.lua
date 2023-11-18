84ec6648fd275eb8b950de4fdb668760faaf6be8
Kenneth Hedman <github@vitberget.se>
2023-11-18 10:05:31 +0100

    folke/flash.nvim

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -24,4 +24,16 @@ return {
     { "folke/twilight.nvim", opts = { } },
     'NvChad/nvim-colorizer.lua',
     "alexghergh/nvim-tmux-navigation",
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            { "<leader>fj", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "<leader>ft", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            -- { "<leader>fr", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            -- { "<leader>fT", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+            -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+        },
+    }
 }
