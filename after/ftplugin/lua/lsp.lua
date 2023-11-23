87b5a4ad5571de2317c383e9f373e1a01db64bb7
Kenneth Hedman <kenneth.hedman@firefly.se>
2023-11-23 16:27:44 +0100

    lsp in separete ftplugin

---
+++ after/ftplugin/lua/lsp.lua
@@ -0,0 +1,2 @@
+local lsp = require('lsp-zero')
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,8 +8,6 @@ lsp.on_attach(function(client, bufnr)
 	lsp.default_keymaps({buffer = bufnr})
 end)
 
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
 local cmp = require('cmp')
 -- local cmp_action = require('lsp-zero').cmp_action()
 cmp.setup({
