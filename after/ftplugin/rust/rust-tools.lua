7ad38818cc5f249c95d9ee02592201cc5af4cbf3
Kenneth Hedman <github@vitberget.se>
2023-12-02 12:24:47 +0100

    new rust bindings

--- after/ftplugin/rust/rust-tools.lua
+++ after/ftplugin/rust/rust-tools.lua
@@ -1,9 +1,12 @@
 vim.keymap.set("n", "<leader>Rr", ":RustRunnables<CR>" )
 vim.keymap.set("n", "<leader>Rd", ":RustDebuggables<CR>" )
 
-vim.keymap.set("n", "<leader>Rmiu", ":RustMoveItemUp<CR>" )
-vim.keymap.set("n", "<leader>Rmid", ":RustMoveItemDown<CR>" )
+vim.keymap.set("n", "<leader>Rmu", ":RustMoveItemUp<CR>" )
+vim.keymap.set("n", "<leader>Rmd", ":RustMoveItemDown<CR>" )
 
-vim.keymap.set("n", "<leader>Rha", ":RustHoverActions<CR>" )
-vim.keymap.set("n", "<leader>Rjl", ":RustJoinLines<CR>" )
+vim.keymap.set("n", "<leader>Rh", ":RustHoverActions<CR>" )
+vim.keymap.set("n", "<leader>Rj", ":RustJoinLines<CR>" )
+
+vim.keymap.set("n", "<leader>Rp", ":RustParentModule<CR>" )
+vim.keymap.set("n", "<leader>Rw", ":RustReloadWorkspace<CR>" )
 
