407b5c83b84a8547cc55e63c24299812bf0bc6bf
Kenneth Hedman <github@vitberget.se>
2023-12-17 00:47:42 +0100

    harpoon 2

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,19 +1,13 @@
-local ui = require("harpoon.ui")
-local mark = require("harpoon.mark")
+local harpoon = require("harpoon")
+harpoon:setup()
 
+vim.keymap.set("n", "<M-1>", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<M-2>", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<M-3>", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<M-4>", function() harpoon:list():select(4) end)
 
-vim.keymap.set("n", "<M-1>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<M-2>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<M-3>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<M-4>", function() ui.nav_file(4) end)
-
-
-vim.keymap.set("n", "<M-0>", function() mark.add_file() end)
-vim.keymap.set("n", "<M-9>", function() ui.toggle_quick_menu() end)
-
-vim.keymap.set("n", "<M-h>", function() ui.nav_prev() end)
-vim.keymap.set("n", "<M-l>", function() ui.nav_next() end)
-
+vim.keymap.set("n", "<M-0>", function() harpoon:list():append() end)
+vim.keymap.set("n", "<M-9>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 require("telescope").load_extension('harpoon')
 vim.keymap.set("n", "<leader>th", ":Telescope harpoon marks<CR>" )
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,10 @@
 return {
     "nvim-lua/plenary.nvim",
-    'ThePrimeagen/harpoon',
     'nvim-telescope/telescope.nvim',
     'nvim-telescope/telescope-ui-select.nvim',
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+
+    }
 }
