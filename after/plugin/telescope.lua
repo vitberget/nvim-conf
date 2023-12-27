def78f99771f272419bfe010b9413ace60159662
Kenneth Hedman <kenneth.hedman@firefly.se>
2023-12-27 17:21:37 +0100

    telescope keys

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,14 +7,13 @@ require("telescope").setup {
 }
 require("telescope").load_extension("ui-select")
 
-
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>tf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>tf', builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set('n', '<leader>ts', builtin.git_status, { desc = "Telescote git status" })
 vim.keymap.set('n', '<leader>tg', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
-vim.keymap.set('n', '<leader>tb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>tr', builtin.registers, {})
-vim.keymap.set('n', '<leader>tw', builtin.lsp_dynamic_workspace_symbols, {})
-vim.keymap.set('n', '<leader>tt', ":TodoTelescope<CR>")
+end, { desc = "Telescope grep" })
+vim.keymap.set('n', '<leader>tb', builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set('n', '<leader>tr', builtin.registers, { desc = "Telescope registers" })
+vim.keymap.set('n', '<leader>tw', builtin.lsp_dynamic_workspace_symbols, { desc = "Telescope workspace symbols" })
+vim.keymap.set('n', '<leader>tt', ":TodoTelescope<CR>", { desc = "Telescope TODO" })
