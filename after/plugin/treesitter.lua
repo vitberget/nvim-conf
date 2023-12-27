20ff8573f929550eec77e03d7fd5eefc996c576f
Kenneth Hedman <github@vitberget.se>
2023-12-27 22:21:13 +0100

    treesitter regex highlighting

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,15 +1,10 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "lua", "vim", "query", "javascript", "rust" },
+  ensure_installed = { "c", "lua", "query", "javascript", "rust" },
   sync_install = false,
   auto_install = true,
 
   highlight = {
     enable = true,
-    additional_vim_regex_highlighting = false,
+    additional_vim_regex_highlighting = true,
   },
---  rainbow = {
---      enable = true,
---      extended_mode = true,
---      max_file_lines = nil,
---  }
 }
