e4e85f65b8d61284ad93a99b99afea7344677382
Kenneth Hedman <github@vitberget.se>
2024-01-10 20:42:23 +0100

    better swapWords

--- after/plugin/swap-words.lua
+++ after/plugin/swap-words.lua
@@ -1,17 +1,18 @@
 local function swapWords()
-    local apos = vim.api.nvim_buf_get_mark(0, "a")
+    local param = vim.fn.getcharstr()
+    local apos = vim.api.nvim_buf_get_mark(0, param)
     local cpos = vim.api.nvim_win_get_cursor(0)
 
     local cword = vim.fn.expand('<cword>')
     vim.api.nvim_win_set_cursor(0, apos)
     local aword = vim.fn.expand('<cword>')
 
-    vim.cmd(":norm diwi" .. cword)
+    vim.cmd(":norm \"_diwi" .. cword)
     vim.api.nvim_win_set_cursor(0, cpos)
 
-    vim.cmd(":norm diwi" .. aword)
+    vim.cmd(":norm \"_diwi" .. aword)
 end
 
 vim.api.nvim_create_user_command('SwapWords', swapWords, {})
 
-vim.keymap.set('n', "<leader>sw", swapWords)
+vim.keymap.set('n', "<leader>sw", swapWords, {})
