52a3964e481f98652cffd3f6ce910d46cac7a67f
Kenneth Hedman <kenneth.hedman@firefly.se>
2024-01-11 08:33:18 +0100

    swap-words improvment

--- init.lua
+++ init.lua
@@ -15,3 +15,5 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
+
+require("vitberget.swap-words")
--- lua/vitberget/swap-words.lua
+++ lua/vitberget/swap-words.lua
@@ -1,16 +1,16 @@
 local function swapWords()
-    local param = vim.fn.getcharstr()
-    local apos = vim.api.nvim_buf_get_mark(0, param)
+    local which_mark = vim.fn.getcharstr()
+    local apos = vim.api.nvim_buf_get_mark(0, which_mark)
     local cpos = vim.api.nvim_win_get_cursor(0)
 
     local cword = vim.fn.expand('<cword>')
     vim.api.nvim_win_set_cursor(0, apos)
     local aword = vim.fn.expand('<cword>')
 
-    vim.cmd(":norm \"_diwi" .. cword)
+    vim.cmd(":norm \"_ciw" .. cword)
     vim.api.nvim_win_set_cursor(0, cpos)
-
-    vim.cmd(":norm \"_diwi" .. aword)
+    vim.cmd(":norm \"_ciw" .. aword)
+    vim.api.nvim_win_set_cursor(0, cpos)
 end
 
 vim.api.nvim_create_user_command('SwapWords', swapWords, {})
