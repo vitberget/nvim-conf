6995377a5a618ecbf7393d30180f523ccc846c36
Kenneth Hedman <kenneth.hedman@firefly.se>
2024-01-18 09:51:16 +0100

    moving rust-tools

--- after/ftplugin/rust/rust-tools.lua
+++ after/ftplugin/rust/rust-tools.lua
@@ -1,12 +1,20 @@
-vim.keymap.set("n", "<leader>Rr", ":RustRunnables<CR>" , {desc="Rust Runnables"})
-vim.keymap.set("n", "<leader>Rd", ":RustDebuggables<CR>" , {desc="Rust Debuggables"})
+local rt = require("rust-tools")
+local mason_registry = require("mason-registry")
 
-vim.keymap.set("n", "<leader>Rmu", ":RustMoveItemUp<CR>", {desc="Rust move item Up"} )
-vim.keymap.set("n", "<leader>Rmd", ":RustMoveItemDown<CR>", {desc="Rust move item Down"} )
+local codelldb_root = mason_registry.get_package("codelldb"):get_install_path() .. "/extension/"
+local codelldb_path = codelldb_root .. "adapter/codelldb"
+local liblldb_path = codelldb_root .. "lldb/lib/liblldb.so"
 
-vim.keymap.set("n", "<leader>Rh", ":RustHoverActions<CR>", {desc="Rust hover actions"})
-vim.keymap.set("n", "<leader>Rj", ":RustJoinLines<CR>" , {desc="Rust join lines"})
+local opts = {
+    server = {
+        on_attach = function(_, bufnr)
+            -- Hover actions
+            --vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            --vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+        end,
+    },
+    dap = { adapter = require('rust-tools.dap').get_codelldb_adapter( codelldb_path, liblldb_path) }
+}
 
-vim.keymap.set("n", "<leader>Rp", ":RustParentModule<CR>", {desc="Rust parent module"} )
-vim.keymap.set("n", "<leader>Rw", ":RustReloadWorkspace<CR>", {desc="Rust reload workspace"} )
-
+rt.setup(opts)
