278a0f7bb6dadd777f20580e2fe21de02ede593d
Kenneth Hedman <github@vitberget.se>
2024-02-02 20:15:46 +0100

    nvim tree on the right

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -1,4 +1,6 @@
-require("nvim-tree").setup {}
+require("nvim-tree").setup {
+    view = { side = "right" }
+}
 
 local api = require("nvim-tree.api")
 vim.keymap.set("n", "<M-`>", api.tree.toggle, { desc = "Nvim tree toggle" })
