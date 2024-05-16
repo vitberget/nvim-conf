d175135ca308f4c476d80eba060c4c4247a9acb5
Kenneth Hedman <kenneth.hedman@firefly.se>
2024-05-16 14:56:44 +0200

    hex.vim do not hexedit latin-1 files (non utf-8?!?)

--- after/plugin/hex.lua
+++ after/plugin/hex.lua
@@ -1,1 +1,6 @@
-require 'hex'.setup()
+require 'hex'.setup({
+    is_file_binary_post_read = function()
+        -- default is_file_binary_post_read checks ifs not utf-8
+        return false
+    end,
+})
