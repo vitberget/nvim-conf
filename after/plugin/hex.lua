b13675c8adb822e676a7e241e778d960e63c5c57
Kenneth Hedman <kenneth.hedman@firefly.se>
2024-05-16 15:47:15 +0200

    no auto hex

--- after/plugin/hex.lua
+++ after/plugin/hex.lua
@@ -1,4 +1,8 @@
 require 'hex'.setup({
+    is_file_binary_pre_read = function()
+        return false
+    end,
+
     is_file_binary_post_read = function()
         -- default is_file_binary_post_read checks ifs not utf-8
         return false
