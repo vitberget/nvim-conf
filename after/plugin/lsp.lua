c8cee069b72088905c4b7fe27720a0695e848ee9
Kenneth Hedman <kenneth.hedman@firefly.se>
2024-05-17 08:23:51 +0200

    keymap descs

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -40,19 +40,28 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = bufnr, remap = false}
-
-	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
-	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
-	vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts)
-	vim.keymap.set("n", "<leader>ch", function() vim.lsp.buf.document_highlight() end, opts)
-	vim.keymap.set("n", "<leader>cH", function() vim.lsp.buf.clear_references() end, opts)
-	vim.keymap.set("n", "<leader>ci", function() vim.lsp.inlay_hint.enable(true) end, opts)
-	vim.keymap.set("n", "<leader>cI", function() vim.lsp.inlay_hint.enable(false) end, opts)
-	vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
-	vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end,
+        { buffer = bufnr, remap = false, desc = "Go to definition" })
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end,
+        { buffer = bufnr, remap = false, desc = "Hover LSP" })
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end,
+        { buffer = bufnr, remap = false, desc = "Prev diagnostic" })
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end,
+        { buffer = bufnr, remap = false, desc = "Next diagnostic" })
+	vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end,
+        { buffer = bufnr, remap = false, desc = "Code action" })
+	vim.keymap.set("n", "<leader>ch", function() vim.lsp.buf.document_highlight() end,
+		{ buffer = bufnr, remap = false, desc = "Document highlight" })
+	vim.keymap.set("n", "<leader>cH", function() vim.lsp.buf.clear_references() end,
+		{ buffer = bufnr, remap = false, desc = "Clear references" })
+	vim.keymap.set("n", "<leader>ci", function() vim.lsp.inlay_hint.enable(true) end,
+		{ buffer = bufnr, remap = false, desc = "Enable inlay hints" })
+	vim.keymap.set("n", "<leader>cI", function() vim.lsp.inlay_hint.enable(false) end,
+		{ buffer = bufnr, remap = false, desc = "Disable inlay hints" })
+	vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end,
+		{ buffer = bufnr, remap = false, desc = "References" })
+	vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end,
+		{ buffer = bufnr, remap = false, desc = "Rename" })
 end)
 
 lsp.setup()
