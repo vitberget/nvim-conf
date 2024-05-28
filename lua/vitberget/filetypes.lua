d1b75f23971a730dd58f70e8e2ee0fff2c39b62f
Kenneth Hedman <kenneth.hedman@firefly.se>
2024-05-28 14:00:54 +0200

    added filetype simfonf, fixed depreceation

--- lua/vitberget/filetypes.lua
+++ lua/vitberget/filetypes.lua
@@ -5,7 +5,18 @@
         pattern = "*.opencl",
         callback = function ()
             local buf = vim.api.nvim_get_current_buf()
-            vim.api.nvim_buf_set_option(buf, "filetype", "opencl")
+            vim.api.nvim_set_option_value("filetype", "opencl", {buf = buf})
+        end
+    }
+  )
+
+  vim.api.nvim_create_autocmd(
+    {"BufNewFile", "BufRead"},
+    {
+        pattern = "*.simconf",
+        callback = function ()
+            local buf = vim.api.nvim_get_current_buf()
+            vim.api.nvim_set_option_value("filetype", "json", {buf = buf})
         end
     }
   )
