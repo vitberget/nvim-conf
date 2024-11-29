d786585339fc6b2b98e385146312ec6adb0d62a2
Kenneth Hedman <github@vitberget.se>
2024-11-29 20:23:35 +0100

    meh

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -150,6 +150,13 @@ return {
             'RainbowDelimQuoted',
             'RainbowMultiDelim'
         }
+    },
+    {
+        'tummetott/unimpaired.nvim',
+        event = 'VeryLazy',
+        opts = {
+            -- add options here if you wish to override the default settings
+        },
     }
 
 }
