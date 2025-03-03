1edf2aec5aea4df942801fb51351109494aa04a7
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-03-03 10:59:31 +0100

    trouble

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -179,4 +179,21 @@ return {
         -- dependencies = { "echasnovski/mini.icons" },
         opts = { }
     },
+    {
+        "folke/trouble.nvim",
+        opts = {}, -- for default options, refer to the configuration section for custom setup.
+        cmd = "Trouble",
+        keys = {
+            {
+                "<leader>bd",
+                "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+                desc = "Buffer Diagnostics (Trouble)",
+            },
+            {
+                "<leader>bs",
+                "<cmd>Trouble symbols toggle focus=false<cr>",
+                desc = "Symbols (Trouble)",
+            },
+        }
+    }
 }
