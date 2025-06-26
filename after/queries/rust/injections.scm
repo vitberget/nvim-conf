8ff85a8efb9047bf65dfb48e001aaeed06e6452b
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-06-26 14:34:12 +0200

    nicer

--- after/queries/rust/injections.scm
+++ after/queries/rust/injections.scm
@@ -7,15 +7,15 @@
 
 ((string_content) 
  @injection.content
- (#lua-match? @injection.content "[Uu][Pp][Dd][Aa][Tt][Ee].*[Ss][Ee][Tt]")
+ (#lua-match? @injection.content "^[Uu][Pp][Dd][Aa][Tt][Ee].*[Ss][Ee][Tt]")
  (#set! injection.language "sql"))
 
 ((string_content) 
  @injection.content
- (#lua-match? @injection.content "[Dd][Ee][Ll][Ee][Tt][Ee].*[Ff][Rr][Oo][Mm]")
+ (#lua-match? @injection.content "^[Dd][Ee][Ll][Ee][Tt][Ee].*[Ff][Rr][Oo][Mm]")
  (#set! injection.language "sql"))
 
 ((string_content) 
  @injection.content
- (#lua-match? @injection.content "[Ii][Nn][Ss][Ee][Rr][Tt].*[Ii][Nn][Tt][Oo]")
+ (#lua-match? @injection.content "^[Ii][Nn][Ss][Ee][Rr][Tt].*[Ii][Nn][Tt][Oo]")
  (#set! injection.language "sql"))
