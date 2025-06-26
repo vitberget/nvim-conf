5992f01f71030461c473382f091ad4c84236d020
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-06-26 09:54:38 +0200

    fixed typos

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -45,9 +45,9 @@ lsp.on_attach(function(client, bufnr)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end,
         { buffer = bufnr, remap = false, desc = "Hover LSP" })
 	vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count=1, float=false}) end,
-        { buffer = bufnr, remap = false, desc = "Prev diagnostic" })
-	vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count=-1, float=false}) end,
         { buffer = bufnr, remap = false, desc = "Next diagnostic" })
+	vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count=-1, float=false}) end,
+        { buffer = bufnr, remap = false, desc = "Prev diagnostic" })
 	vim.keymap.set("n", "<leader>co", function() vim.diagnostic.open_float() end,
         { buffer = bufnr, remap = false, desc = "Diagnostic popup" })
 	vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end,
