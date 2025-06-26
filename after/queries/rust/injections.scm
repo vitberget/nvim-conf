209c291a21025132167d53f253e9de6da52abd3c
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-06-26 14:29:08 +0200

    fix rust sql treesitter injection

--- after/queries/rust/injections.scm
+++ after/queries/rust/injections.scm
@@ -1,35 +1,29 @@
 ;; extends
 
-;; SQL string_literal
-((string_literal) @sql
-    (#lua-match? @sql "^\"[Ss][Ee][Ll][Ee][Cc][Tt].*[Ff][Rr][Oo][Mm]")
-    (#offset! @sql 0 1 0 -1))
+((string_content) 
+ @injection.content
+ (#lua-match? @injection.content "^[Ss][Ee][Ll][Ee][Cc][Tt].*[Ff][Rr][Oo][Mm]")
+ (#set! injection.combined)
+ (#set! injection.language "sql")
+ )
 
-((string_literal) @sql
-    (#lua-match? @sql "^\"[Uu][Pp][Dd][Aa][Tt][Ee].*[Ss][Ee][Tt]")
-    (#offset! @sql 0 1 0 -1))
+((string_content) 
+ @injection.content
+ (#lua-match? @injection.content "[Uu][Pp][Dd][Aa][Tt][Ee].*[Ss][Ee][Tt]")
+ (#set! injection.combined)
+ (#set! injection.language "sql")
+ )
 
-((string_literal) @sql
-    (#lua-match? @sql "^\"[Dd][Ee][Ll][Ee][Tt][Ee]%s*[Ff][Rr][Oo][Mm]")
-    (#offset! @sql 0 1 0 -1))
+((string_content) 
+ @injection.content
+ (#lua-match? @injection.content "[Dd][Ee][Ll][Ee][Tt][Ee].*[Ff][Rr][Oo][Mm]")
+ (#set! injection.combined)
+ (#set! injection.language "sql")
+ )
 
-((string_literal) @sql
-    (#lua-match? @sql "^\"[Ii][Nn][Ss][Ee][Rr][Tt]%s*[Ii][Nn][Tt][Oo]")
-    (#offset! @sql 0 1 0 -1))
-
-;; SQL raw_string_literal
-((raw_string_literal) @sql
-    (#lua-match? @sql "^r#\"[Ss][Ee][Ll][Ee][Cc][Tt].*[Ff][Rr][Oo][Mm]")
-    (#offset! @sql 0 3 0 -2))
-
-((raw_string_literal) @sql
-    (#lua-match? @sql "^r#\"[Uu][Pp][Dd][Aa][Tt][Ee].*[Ss][Ee][Tt]")
-    (#offset! @sql 0 3 0 -2))
-
-((raw_string_literal) @sql
-    (#lua-match? @sql "^r#\"[Dd][Ee][Ll][Ee][Tt][Ee]%s*[Ff][Rr][Oo][Mm]")
-    (#offset! @sql 0 3 0 -2))
-
-((raw_string_literal) @sql
-    (#lua-match? @sql "^r#\"[Ii][Nn][Ss][Ee][Rr][Tt]%s*[Ii][Nn][Tt][Oo]")
-    (#offset! @sql 0 3 0 -2))
+((string_content) 
+ @injection.content
+ (#lua-match? @injection.content "[Ii][Nn][Ss][Ee][Rr][Tt].*[Ii][Nn][Tt][Oo]")
+ (#set! injection.combined)
+ (#set! injection.language "sql")
+ )
