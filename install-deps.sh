976f645e50e29551c65bd5d209be6c5c290879f5
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-06-30 11:42:46 +0200

    install deps fzf

--- install-deps.sh
+++ install-deps.sh
@@ -4,11 +4,11 @@ OS_ID=$(cat /etc/os-release | grep "^ID")
 
 case "$OS_ID" in
     "ID=arch")      
-        sudo pacman -S git tinyxxd ripgrep
+        sudo pacman -S git tinyxxd ripgrep fzf
         ;;
 
     "ID=debian")
-        sudo apt install git xxd ripgrep
+        sudo apt install git xxd ripgrep fzf
         ;;
 
     *)              
