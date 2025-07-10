bee04a6ac246253f910c031a9a976455febdae82
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-07-10 11:03:23 +0200

    add back some lsp binds

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,19 @@
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        -- local bufnr = event.buf
+        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end,
+        { buffer = args.buf, remap = false, desc = "Go to definition" })
+
+        vim.keymap.set("n", "<leader>co", function() vim.diagnostic.open_float() end,
+        { buffer = args.buf, remap = false, desc = "Diagnostic popup" })
+
+        vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end,
+        { buffer = args.buf, remap = false, desc = "Code action" })
+
+        vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end,
+        { buffer = args.buf, remap = false, desc = "References" })
+
+        vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end,
+        { buffer = args.buf, remap = false, desc = "Rename" })
+    end,
+})
