e54770ec9dbad0f3dd2e6fa49ef9dcc799b15991
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-07-21 14:57:42 +0200

    lsp autocomp bind

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -20,8 +20,8 @@ vim.api.nvim_create_autocmd('LspAttach', {
         local client = vim.lsp.get_client_by_id(args.data.client_id)
         if client:supports_method('textDocument/completion') then
             vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = false })
-            vim.keymap.set("i", "<c-m>", function() vim.lsp.completion.get() end,
-            { buffer = args.buf, remap = false, desc = "References" })
+            vim.keymap.set("i", "<c-x><c-x>", function() vim.lsp.completion.get() end,
+            { buffer = args.buf, remap = false, desc = "Autocomplete Code" })
         end
     end,
 })
