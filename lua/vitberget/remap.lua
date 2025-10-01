b9c96ae7ce2b1bc740f7ce99788cffef6abc2d77
Kenneth Hedman <github@vitberget.se>
2025-10-01 22:53:41 +0200

    bind bdelete

--- lua/vitberget/remap.lua
+++ lua/vitberget/remap.lua
@@ -50,3 +50,4 @@ vim.keymap.set("n", "<Leader>S", ":sort<CR>", {desc="Sort"})
 vim.keymap.set("n", "<Leader>vr", ":hi Visual term=reverse cterm=reverse gui=reverse<CR>", {desc="Visual Reverse"})
 
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<Leader>bx', ':bdelete<CR>', { desc = 'Buffer Delete' })
