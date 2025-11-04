89d2715491edb315161d51b47193fb5c4ca86d15
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-11-04 16:51:22 +0100

    when commenting linewise, move cursor down

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -1,9 +1,20 @@
-require('Comment').setup()
+require('Comment').setup {
+    post_hook = function(ctx)
+        if ctx.range.srow == ctx.range.erow then
+            local r = unpack(vim.api.nvim_win_get_cursor(0))
+            local rcnt = vim.api.nvim_buf_line_count(0)
 
-local call = require('Comment.api').call
+            if rcnt > r then
+                vim.api.nvim_win_set_cursor(0, {r+1,0})
+            end
+        end
+    end,
+}
+
+local api = require('Comment.api')
 
 vim.keymap.set( 'n', "<leader>/",
-    call('toggle.linewise.current', 'g@$'),
+    api.call('toggle.linewise.current', 'g@$'),
     { expr = true, desc = 'Comment toggle linewise' })
 
 vim.keymap.set( 'x', "<leader>/",
